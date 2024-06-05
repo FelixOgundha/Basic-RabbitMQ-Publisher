525b10469a6eb9748b5647c07836be527123eb3a
fogundha <fogundha@strathmore.edu>
2024-06-05 15:12:48 +0300

    Add project files.

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+        queue: "Hello",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+
+const string message = "Hello World message";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: "hello",
+        basicProperties: null,
+        body: body
+    );
